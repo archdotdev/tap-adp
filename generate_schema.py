44ad0c700698f9a2a52ddccff522c3cb0f359a3e
Sebastian Smiley <sebastianswms@gmail.com>
2024-12-20 08:53:33 -0500

    requisitions and applications

--- generate_schema.py
+++ generate_schema.py
@@ -2,7 +2,7 @@ import json
 import os
 from genson import SchemaBuilder
 from tap_adp.tap import TapADP
-from tap_adp.streams import WorkersStream, PayrollInstructionStream, PayDataInputStream, USTaxProfileStream
+from tap_adp.streams import JobRequisitionStream, JobApplicationStream
 from dotenv import load_dotenv
 from decimal import Decimal
 
@@ -67,22 +67,13 @@ def main():
     tap = TapADP(config=config)
 
     # Configure this part as-needed for the streams you want to generate schemas for.
-    parent = WorkersStream(tap=tap)
     children = {
-        "payroll_instruction": PayrollInstructionStream(tap=tap),
-        "pay_data_input": PayDataInputStream(tap=tap),
-        "us_tax_profile": USTaxProfileStream(tap=tap),
+        "job_requisition": JobRequisitionStream(tap=tap),
+        "job_application": JobApplicationStream(tap=tap),
     }
 
-
-    gen = parent.get_records(context=None)
-    first = next(gen, None)
-    if not first:
-        raise RuntimeError("no parent entries found")
-    context = parent.get_child_context(record=first, context=None)
-
     for child_name, child_stream in children.items():
-        generate_schema(child_stream, context=context, output_file=f'tap_adp/schemas/{child_name}.json')
+        generate_schema(child_stream, context=None, output_file=f'tap_adp/schemas/{child_name}.json')
 
 if __name__ == '__main__':
     main()
